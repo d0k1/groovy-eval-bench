ccb4ebcefdd3125fb18c51dd20c89dc18683b92a
dOkI <denis.kirpichenkov@gmail.com>
2016-05-30 09:31:47 +0500

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+There is some jmh benchmarks I wrote to find out which way is the best to call groovy scripts and/or groovy's template engine
---
+++ src/main/resources/SimpleScript.groovy
@@ -0,0 +1,12 @@
+/**
+ * Created by doki on 30.05.16.
+ */
+
+def a = 10.25;
+def b = 2;
+
+def x = 122;
+
+def y = a * x + b + key;
+
+return y;
